cab5f7a0b5b3547793b1916fb0cbabf7fa7f879d
Joshua Porter <joshporter1@gmail.com>
2018-06-06 21:53:41 -0400

    Moved mail creds to env

--- helpers.js
+++ helpers.js
@@ -7,35 +7,34 @@ function extractListings(html) {
   const homeRows = $('#dgPropertyList tbody tr.FormTableRow, #dgPropertyList tbody tr.FormTableRowAlt');
 
   const homes = [];
+  const newHomes = []
   homeRows.each((i, el) => {
     // scrape necessary data from each row and push to array
     // grab bidOpenDate. listings page does not have list date.
     // new listings usually have a bidOpenDate 7 days after listing date
-    let bidOpenDate = $(el).children('td:nth-child(9)').first().text().trim();
-    let isNew = moment(bidOpenDate).subtract(7, 'days').isSame(moment(), 'day')
-    // only add "new" homes
-    if (isNew) {
-      // TODO: scrape these better..
-      let id = $(el).children('td:nth-child(2)').first().text().trim();
-      let address = $($(el).children('td:nth-child(3)').first().html().replace(/<br>/g, ' ')).text().trim()
-      let price = $(el).children('td:nth-child(4)').first().text().trim();
-      let beds = $(el).children('td:nth-child(6)').first().text().trim();
-      let baths = $(el).children('td:nth-child(7)').first().text().trim();
-      let status = $(el).find('td:nth-child(5) img').first().attr('title');
-      let link = $(el).find('td:nth-child(2) a').first().attr('onclick').replace('top.location.href=\'', '').replace('\';', '');
-      homes.push({
-        address,
-        price,
-        beds,
-        baths,
-        bidOpenDate,
-        status,
-        link
-      })
+    // TODO: scrape these better
+    let scrapes = {
+      id: $(el).children('td:nth-child(2)').first().text().trim(),
+      address:  $($(el).children('td:nth-child(3)').first().html().replace(/<br>/g, ' ')).text().trim(),
+      price:  $(el).children('td:nth-child(4)').first().text().trim(),
+      beds:  $(el).children('td:nth-child(6)').first().text().trim(),
+      baths:  $(el).children('td:nth-child(7)').first().text().trim(),
+      status:  $(el).find('td:nth-child(5) img').first().attr('title'),
+      link:  $(el).find('td:nth-child(2) a').first().attr('onclick').replace('top.location.href=\'', '').replace('\';', ''),
+      bidOpenDate: $(el).children('td:nth-child(9)').first().text().trim()
     }
+    // add to newHomes array
+    let listingDate = moment(scrapes.bidOpenDate, 'MM/DD/YYYY').subtract(7, 'days')
+    scrapes.listingDate = listingDate.format('MM/DD/YYYY')
+    if (listingDate.isSame(moment(), 'day')) {
+      newHomes.push(scrapes)
+    }
+    homes.push(scrapes)
   });
-  // fire off email
-  mailListings(homes);
+  // fire off email if there are any new homes
+  if (newHomes.length > 0){
+    mailListings(newHomes);
+  }
   return homes;
 }
 
@@ -49,8 +48,9 @@ function listingsTable(homes) {
         <th style="vertical-align: bottom; ${cellStyle}">Price</th>
         <th style="vertical-align: bottom; ${cellStyle}">Beds</th>
         <th style="vertical-align: bottom; ${cellStyle}">Baths</th>
-        <th style="vertical-align: bottom; ${cellStyle}">Bid Open Date</th>
         <th style="vertical-align: bottom; ${cellStyle}">Status</th>
+        <th style="vertical-align: bottom; ${cellStyle}">Bid Open Date</th>
+        <th style="vertical-align: bottom; ${cellStyle}">Listing Date</th>
         <th style="vertical-align: bottom; ${cellStyle}"></th>
       </tr>
     </thead>
@@ -59,8 +59,8 @@ function listingsTable(homes) {
     let link = home.link
     delete home.link
     html += `<tr>`
-    Object.values(home).forEach((val) => {
-      html += `<td style="vertical-align: top; ${cellStyle}">${val}</td>`
+    Object.keys(home).forEach((k) => {
+      html += `<td style="vertical-align: top; ${cellStyle}">${home[k]}</td>`
     })
     html += `<td style="vertical-align: top; ${cellStyle}">
       <a href='${link}' target='_BLANK'>View Listing</a>
@@ -72,43 +72,32 @@ function listingsTable(homes) {
 
 function mailListings(homes) {
   let messageHTML = listingsTable(homes);
-
-  // TODO: Set up a real mailer
-
-  // Generate test SMTP service account from ethereal.email
-  // Only needed if you don't have a real mail account for testing
-  nodemailer.createTestAccount((err, account) => {
-    // create reusable transporter object using the default SMTP transport
-    let transporter = nodemailer.createTransport({
-      host: 'smtp.ethereal.email',
-      port: 587,
-      secure: false, // true for 465, false for other ports
-      auth: {
-        user: account.user, // generated ethereal user
-        pass: account.pass // generated ethereal password
-      }
-    });
-
-    // setup email data with unicode symbols
-    let mailOptions = {
-      from: '"Fred Foo 👻" <foo@example.com>', // sender address
-      to: 'joshporter1@gmail.com', // list of receivers
-      subject: 'New HUDHome Listings Available! 🏚🏠', // Subject line
-      text: JSON.stringify(homes), // plain text body
-      html: messageHTML // html body
-    };
-
-    // send mail with defined transport object
-    transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-        return console.log(error);
-      }
-      console.log('Message sent: %s', info.messageId);
-      // Preview only available when sending through an Ethereal account
-      console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-    });
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true, // use SSL
+    auth: {
+      user: process.env.MAILER_USER, // generated ethereal user
+      pass: process.env.MAILER_PASS // generated ethereal password
+    }
   });
 
+  // setup email data with unicode symbols
+  let mailOptions = {
+    from: '"JPorter Mailer" <jporter.mailer@gmail.com>', // sender address
+    to: 'joshporter1@gmail.com', // list of receivers
+    subject: 'New HUDHome Listings Available! 🏚🏠', // Subject line
+    text: JSON.stringify(homes), // plain text body
+    html: messageHTML // html body
+  };
+  // send mail
+  transporter.sendMail(mailOptions, function (err, info) {
+    if(err)
+      console.log(err)
+    else
+      console.log(info);
+  });
 }
 
 module.exports = {
